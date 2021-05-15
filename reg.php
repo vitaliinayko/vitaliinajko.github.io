3d3fce0863ae487f094a7e3167a793d3b0f4336e
vitaliinayko <84187459+vitaliinayko@users.noreply.github.com>
2021-05-15 09:11:43 +0300

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,34 @@
+<?php 
+    $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
+
+    $psw = filter_var(trim($_POST['psw']), FILTER_SANITIZE_STRING);
+
+    $pswrepeat = filter_var(trim($_POST['psw-repeat']), FILTER_SANITIZE_STRING);
+
+    if(mb_strlen($name) < 2 || mb_strlen($name) > 20){
+        echo "Убедитесь в правильности написании имени";
+        exit();
+    }
+    else if(mb_strlen($email) < 4 || mb_strlen($email) > 25){
+        echo "Убедитесь в правильности написании почты";
+        exit();
+    }
+    else if(mb_strlen($psw) < 7 || mb_strlen($psw) > 25){
+        echo "Убедитесь в правильности написании пароля";
+        exit();
+    }
+
+    if($psw != $pswrepeat){
+        echo "Пароли не совпадают";
+        exit();
+    }
+
+    $mysql = new mysqli('localhost', 'mysql', 'mysql', 'sql_mus');
+    $mysql->query("INSERT INTO `music` (`name`, `mail`, `password`) VALUES('$name', '$email', '$psw')");
+
+    $mysql->close();
+
+    header('Location: /');
+?>
