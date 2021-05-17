99c080fcf4643e145928bab74ad85b1d53d85b0e
vitaliinayko <84187459+vitaliinayko@users.noreply.github.com>
2021-05-17 23:23:50 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,4 +1,5 @@
 <?php 
+    http_response_code(405);
     $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
 
     $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
